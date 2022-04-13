397b86cf4819bfabec25dbe7831ba76790f0a967
Sergey <46491304+mrflusha@users.noreply.github.com>
2022-04-13 16:24:32 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+﻿import os
+import subprocess
+
+
+path, path_to = input("Enter path:\n"),input("Enter path_to:\n")
+print("docto.exe -f \"" + path + "\" -O \"" + path_to + "\" -T wdFormatPDF  -OX .pdf")
+
+def convert(path, path_to):
+	a = subprocess.Popen("docto.exe -f \"" + path + "\" -O \"" + path_to + "\" -T wdFormatPDF  -OX .pdf", stdout=subprocess.PIPE, shell=True)
+	out = a.stdout.read()
+	print(out)
+	#print (out.decode('utf-8'))
+
+convert(path, path_to)
